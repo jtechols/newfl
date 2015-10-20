93846f86e4e723dd9d8b903c5d0b88d7122aeb03
Jack Echols <jack.echols96@gmail.com>
2015-10-20 01:05:15 -0700

    change static s3

--- nfl/urls.py
+++ nfl/urls.py
@@ -21,7 +21,3 @@ urlpatterns = [
 	url(r'^shb/', include('shb.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
