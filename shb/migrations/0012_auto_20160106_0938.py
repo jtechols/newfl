47f4c759903752fc247f65f17d8cafc9438ae1ed
Jack Echols <jack.echols96@gmail.com>
2016-01-06 09:38:36 -0800

    maybe?

---
+++ shb/migrations/0012_auto_20160106_0938.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('shb', '0011_auto_20160104_1457'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='newman',
+            name='flex',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name='newman',
+            name='saxophone',
+            field=models.BooleanField(default=False),
+        ),
+    ]
