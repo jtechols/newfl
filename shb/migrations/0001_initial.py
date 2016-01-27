20223e2e982d74180dd4567e5d3bcc977304e5b1
Jack Echols <jack.echols96@gmail.com>
2016-01-26 21:07:23 -0800

    re did migrations

--- shb/migrations/0001_initial.py
+++ shb/migrations/0001_initial.py
@@ -13,43 +13,27 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Newman',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
                 ('newman_name', models.CharField(max_length=200)),
                 ('newman_instrument', models.CharField(max_length=100)),
-                ('points', models.IntegerField(default=0)),
-                ('imgFileName', models.CharField(max_length=200, null=True)),
-                ('woodwind', models.BooleanField(default=False)),
-                ('saxophone', models.BooleanField(default=False)),
-                ('lowbrass', models.BooleanField(default=False)),
-                ('highbrass', models.BooleanField(default=False)),
-                ('perc', models.BooleanField(default=False)),
-                ('bench', models.BooleanField(default=False)),
-                ('flex', models.BooleanField(default=False)),
             ],
         ),
         migrations.CreateModel(
             name='Oldmen',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
                 ('team_name', models.CharField(max_length=200)),
                 ('team_owner', models.CharField(max_length=200)),
-                ('team_points', models.IntegerField(default=0)),
-                ('locked', models.BooleanField(default=False)),
+                ('newman', models.ForeignKey(to='shb.Newman')),
             ],
         ),
         migrations.CreateModel(
             name='SHB',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
                 ('shb_name', models.CharField(max_length=200)),
                 ('shb_time', models.DateTimeField(verbose_name='Date of SHB')),
-                ('limited', models.BooleanField(default=False)),
-                ('newman_list', models.ManyToManyField(to='shb.Newman')),
+                ('newman', models.ManyToManyField(to='shb.Newman')),
             ],
-        ),
-        migrations.AddField(
-            model_name='newman',
-            name='owner',
-            field=models.ForeignKey(null=True, blank=True, to='shb.Oldmen'),
         ),
     ]
