d5f576063e971a9d3a94e9fb280842bc9231fcbe
Jack Echols <jack.echols96@gmail.com>
2016-01-27 11:03:10 -0800

    lock_lineup syntax fix

--- shb/views.py
+++ shb/views.py
@@ -196,5 +196,5 @@ def flex_newman(request, newman_id):
 def lock_lineups(request):
 	if request.user.is_staff():
 		for old in Oldmen.objects.all():
-			old.locked = True:
+			old.locked = True
 	return mySHB(request):
